ae4300da9e63e20660dc3451b51a4b3bd77c9f3b
devictoralmeida <victoremmanuelmn@gmail.com>
2023-12-06 18:21:35 -0300

    pre deployment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  domain: 'http://localhost',
+  api_key: '67c3fb09e5bafdb447fe58c090e4b0aa',
+  api_token:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2MzZmIwOWU1YmFmZGI0NDdmZTU4YzA5MGU0YjBhYSIsInN1YiI6IjYzZDE1NGE3NjZhZTRkMDA5NmI2OTIzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7PseDC4hwUm2Wz8fiMVUZKmmvyLNjMjTAkPPpwg9--0',
+};
